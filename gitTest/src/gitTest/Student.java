4725ae3593c29634209a1744c68bc2bf914c3fb3
Clarissa Baciu <clarissa.baciu@mail.mcgill.ca>
2021-01-31 11:16:38 -0500

    start student development

---
+++ gitTest/src/gitTest/Student.java
@@ -0,0 +1,20 @@
+package gitTest;
+
+public class Student {
+	
+	int id; 
+	int name; 
+	
+	public int getId() {
+		return id; 
+	}
+	public int getName() {
+		return name; 
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
