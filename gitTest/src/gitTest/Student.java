28cd80b48951615ee989a78186a980e663f0e7e8
Clarissa Baciu <clarissa.baciu@mail.mcgill.ca>
2021-01-31 11:28:06 -0500

    add constructor to Student

--- gitTest/src/gitTest/Student.java
+++ gitTest/src/gitTest/Student.java
@@ -5,16 +5,23 @@ public class Student {
 	int id; 
 	int name; 
 	
+	public Student (int id, int name) {
+		this.id = id;
+		this.name = name;
+	}
 	public int getId() {
 		return id; 
 	}
 	public int getName() {
 		return name; 
 	}
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
 
 }
